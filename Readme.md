5847478e35345903e1a18bacc9516fbd775f57a1
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2023-06-29 21:46:39 +0530

    VueJs basics

--- Readme.md
+++ Readme.md
@@ -1,10 +1,8 @@
 ## VUE LEARNING
 
 ### Concepts Covered
-- Interpolation and Data binding
-- Binding Attributes with the v-bind Directive
-- Understanding Methods in Vue
-- HTML content output dynamically (v-html Directive)
-- Events & Methods
-- Event Modifiers (e.prevent.default)
-- Locking Content with v-once
+- Data Binding + Event Binding = Two Way Binding (v-bind and v-on  or v-model)
+- Computed Properties
+- Working with watchers (HTTPS Request / Timer)
+- v-on:click shorthand == >  @click
+- v-bind:value ===>  :value
--- app.js
+++ app.js
@@ -1,57 +1,37 @@
 // Creating Vue App
 const app = Vue.createApp({
-    data(){
-        return {
-            title1: `Learn VueJs`,
-            title2: `Master VueJs`,
-            vueLink: 'https://vuejs.org/',
-            htmlContent: `<h1>HTML CONTENT</h1>`,
-            counter:0,
-            counter1:0,
-            counter2:0,
-            name:'',
-
-        }
+  data() {
+    return {
+      name: "",
+      fullName: "",
+    };
+  },
+  watch: {
+    name(newValue, oldValue) {
+      if (newValue === "") {
+        this.fullName = "";
+      } else {
+        // this.fullName = this.name + " " + "TESTING !!!!";
+        this.fullName = newValue + " " + "TESTING !!!!";
+      }
     },
-    methods:{
-        dynamicTitle(){
-            const randomNumber = Math.random();
-            if(randomNumber < 0.5){
-                return this.title1;
-            }else{
-                return this.title2;
-            }
-        },
-        add(){
-            this.counter1 = this.counter1 +2;
-
-        },
-        reduce(){
-            this.counter1 = this.counter1 -2;
-
-        },
-        addv(val){
-            this.counter2 = this.counter2 +val;
-
-        },
-        reducev(val){
-            this.counter2 = this.counter2 -val;
-
-        },
-        setName(event,suffix){
-            this.name = event.target.value + suffix;
-
-        },
-        submitForm(event){
-            event.preventDefault();
-            alert("Testing");
-        },
-
-        submitForm1(event){
-            alert("Testing-1");
-        }
-    }
+  },
+  computed: {
+    // fullName(){
+    //     if(this.name === ''){
+    //         return '';
+    //     }
+    //     return this.name + "TESTING!!";
+    // }
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    resetName() {
+      this.name = "";
+    },
+  },
 });
 
-
-app.mount('#app');
+app.mount("#app");
