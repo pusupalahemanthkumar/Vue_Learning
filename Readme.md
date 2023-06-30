142aa721e674cad978b07a410a7154ecf594842a
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2023-06-30 18:52:54 +0530

    lists and conditions rendering

--- Readme.md
+++ Readme.md
@@ -1,4 +1,7 @@
 ## VUE LEARNING
 
 ### Concepts Covered
-- Dynamic Styling
+- Rendering content with conditions (v-if , v-else , v-else-if )
+- v-show instead of v-if (v-show set display:none for element if condition is false , where as v-if removes it from dom)
+- Outputting lists of data (v-for )
+- V-for ==> Arrays , Objects, Range of numbers
--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 const app = Vue.createApp({
   data() {
-    return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false,
-    };
-  },
-  computed: {
-    boxAClasses() {
-      return { active: this.boxASelected };
-    },
+    return { 
+      enteredGoalValue: '',
+      goals: []
+     };
   },
   methods: {
-    boxSelected(box) {
-      if (box === 'A') {
-        this.boxASelected = !this.boxASelected;
-      } else if (box === 'B') {
-        this.boxBSelected = !this.boxBSelected;
-      } else if (box === 'C') {
-        this.boxCSelected = !this.boxCSelected;
-      }
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
     },
-  },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
+    }
+  }
 });
 
-app.mount('#styling');
+app.mount('#user-goals');
