95b46fcdef1e6c706d1bebf551264f309b0856c0
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2023-06-30 19:56:22 +0530

    Vue behind scenes

--- Readme.md
+++ Readme.md
@@ -1,7 +1,8 @@
 ## VUE LEARNING
 
 ### Concepts Covered
-- Rendering content with conditions (v-if , v-else , v-else-if )
-- v-show instead of v-if (v-show set display:none for element if condition is false , where as v-if removes it from dom)
-- Outputting lists of data (v-for )
-- V-for ==> Arrays , Objects, Range of numbers
+- How Vue works internally ? (Virtual DOM and proxy mechanism)
+- one App vs Mutiple Apps (Multiple Vue.createApp())
+- Understanding templates
+- Working with ref 
+- Vue App LifeCycle
--- app.js
+++ app.js
@@ -1,18 +1,85 @@
 const app = Vue.createApp({
   data() {
-    return { 
-      enteredGoalValue: '',
-      goals: []
-     };
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
   },
   methods: {
-    addGoal() {
-      this.goals.push(this.enteredGoalValue);
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
     },
-    removeGoal(idx) {
-      this.goals.splice(idx, 1);
-    }
-  }
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      // console.dir(this.$refs.userText);
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  },
 });
 
-app.mount('#user-goals');
+app.mount('#app');
+
+// setTimeout(function () {
+//   app.unmount();
+// }, 3000);
+
+const app2 = Vue.createApp({
+  template: `
+    <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: 'Pizza',
+    };
+  },
+});
+
+app2.mount('#app2');
+
+
+
+// PROXY CONCEPT IN JAVASCRIPT
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!';
+
+// console.log(proxy.longMessage);
